210c824971ea9480a5adb50f0b1b582250bee8d7
kazuaki-aso-rp <kazuaki.aso@rakus-partners.co.jp>
2022-03-03 14:16:47 +0900

    注文確認画面及び処理コード一時保存

---
+++ src/main/java/com/example/ecommerce_a/form/OrderConfirmForm.java
@@ -0,0 +1,77 @@
+package com.example.ecommerce_a.form;
+
+public class OrderConfirmForm {
+	String destinationName;
+	String destinationEmail;
+	String destinationZipcode;
+	String destinationAddress;
+	String destinationTel;
+	String deliveryDate;
+	String deliveryHour;
+	String paymentMethod;
+
+	public String getDestinationName() {
+		return destinationName;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public String getDestinationEmail() {
+		return destinationEmail;
+	}
+
+	public void setDestinationEmail(String destinationEmail) {
+		this.destinationEmail = destinationEmail;
+	}
+
+	public String getDestinationZipcode() {
+		return destinationZipcode;
+	}
+
+	public void setDestinationZipcode(String destinationZipcode) {
+		this.destinationZipcode = destinationZipcode;
+	}
+
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+
+	public String getDestinationTel() {
+		return destinationTel;
+	}
+
+	public void setDestinationTel(String destinationTel) {
+		this.destinationTel = destinationTel;
+	}
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getDeliveryHour() {
+		return deliveryHour;
+	}
+
+	public void setDeliveryHour(String deliveryHour) {
+		this.deliveryHour = deliveryHour;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+}
