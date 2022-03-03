d612fe42e4d2b7c7acd09d733e99ff642b511d06
shimon <shimon.itakura@rakus-partners.co.jp>
2022-03-03 16:18:40 +0900

    商品一覧List

---
+++ src/main/java/com/example/ecommerce_a/controller/ItemController.java
@@ -0,0 +1,38 @@
+package com.example.ecommerce_a.controller;
+
+import java.util.List;
+
+import com.example.ecommerce_a.domain.Item;
+import com.example.ecommerce_a.service.ItemService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shoppingList")
+public class ItemController {
+    @Autowired
+    private ItemService service;
+
+
+
+    @RequestMapping("")
+    public String index(Model model){
+        List<Item> itemList=service.findAll();
+        model.addAttribute("itemList", itemList);
+        return "item_list_coffee";
+    }
+    @RequestMapping("/searchWord")
+    public String findByLikeWord(String searchWord,Model model){
+        List<Item> itemList=service.findByLikeName(searchWord);
+        if(itemList.size()==0){
+            String nullMessage="該当する商品がありません";
+            model.addAttribute("nullMessage", nullMessage);
+            return "redirect:/shoppingList";
+        }
+        model.addAttribute("itemList", itemList);
+        return "item_list_coffee";
+    }
+}
