bcfb3ffcecd2ef37bf31195285f90af0f50891af
shimon0 <shimon.itakura@rakus-partners.co.jp>
2022-03-03 11:26:58 +0900

    domain kari

---
+++ src/main/java/com/example/ecommerce_a/domain/OrderTopping.java
@@ -0,0 +1,34 @@
+package com.example.ecommerce_a.domain;
+
+public class OrderTopping {
+	private Integer id;
+	private Integer toppingId;
+	private Integer orderItemId;
+	private Topping topping;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getToppingId() {
+		return toppingId;
+	}
+	public void setToppingId(Integer toppingId) {
+		this.toppingId = toppingId;
+	}
+	public Integer getOrderItemId() {
+		return orderItemId;
+	}
+	public void setOrderItemId(Integer orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+	public Topping getTopping() {
+		return topping;
+	}
+	public void setTopping(Topping topping) {
+		this.topping = topping;
+	}
+	
+	
+}
--- src/main/java/com/example/ecommerce_a/domain/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.example.ecommerce_a.domain;
