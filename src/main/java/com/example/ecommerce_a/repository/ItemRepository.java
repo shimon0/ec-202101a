df909992935ec2ac622457a5faf2483dee928b9c
shimon <shimon.itakura@rakus-partners.co.jp>
2022-03-04 11:33:48 +0900

    商品一覧仮作成

--- src/main/java/com/example/ecommerce_a/repository/ItemRepository.java
+++ src/main/java/com/example/ecommerce_a/repository/ItemRepository.java
@@ -21,14 +21,25 @@ public class ItemRepository {
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
-	private static final RowMapper<Item> ITEM_ROW_MAPPER=new BeanPropertyRowMapper<>(Item.class);
+	private static final RowMapper<Item> ITEM_ROW_MAPPER=(rs,i)->{
+		Item item=new Item();
+		item.setId(rs.getInt("id"));
+		item.setName(rs.getString("name"));
+		item.setImagePath(rs.getString("image_path"));
+		item.setPriceM(rs.getInt("price_m"));
+		item.setPriceL(rs.getInt("price_l"));
+		item.setDescription(rs.getString("description"));
+
+		return item;
+
+	};
 	
 	/**
 	 * 全件検索を行う
 	 * @return 商品一覧
 	 */
 	public List<Item> findAll(){
-		String sql="SELECT name,price_m,price_l,image_path FROM items ORDER BY  price_m  ASC ;";
+		String sql="SELECT id,name,price_m,price_l,image_path,description FROM items ORDER BY  price_m  ASC ;";
 		List<Item> itemList=template.query(sql, ITEM_ROW_MAPPER);
 		
 		return itemList;
@@ -40,14 +51,14 @@ public class ItemRepository {
 	 * @return　商品一覧
 	 */
 	public List<Item> findByLikeName(String searchWord) {
-		String sql="SELECT name,price_m,price_l,image_path FROM items WHERE name like :name;";
+		String sql="SELECT id,name,price_m,price_l,image_path,description FROM items WHERE name like :name;";
 		SqlParameterSource param=new MapSqlParameterSource().addValue("name", "%"+searchWord+"%");
 		List<Item> itemList=template.query(sql, param, ITEM_ROW_MAPPER);
 		return itemList;
 	}
 	
 	public Item load(Integer id) {
-		String sql="SELECT name,  price_m,price_l,image_path,description FROM items WHERE id=:id";
+		String sql="SELECT id,name,  price_m,price_l,image_path,description FROM items WHERE id=:id";
 		SqlParameterSource param=new MapSqlParameterSource().addValue("id", id);
 		Item item=template.queryForObject(sql, param, ITEM_ROW_MAPPER);
 		return item;
