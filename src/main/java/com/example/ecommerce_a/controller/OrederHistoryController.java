2d65824f7f9dc13790415eaa1dd2edf8180dda82
shimon <shimon.itakura@rakus-partners.co.jp>
2022-03-08 13:45:28 +0900

    仮作成

--- src/main/java/com/example/ecommerce_a/controller/OrederHistoryController.java
+++ src/main/java/com/example/ecommerce_a/controller/OrederHistoryController.java
@@ -1,6 +1,11 @@
 package com.example.ecommerce_a.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
+
+import com.example.ecommerce_a.domain.Order;
+import com.example.ecommerce_a.service.OrderHistoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,17 +18,22 @@ public class OrederHistoryController {
     @Autowired
     private HttpSession session;
 
+    @Autowired
+    private OrderHistoryService service;
+
     @RequestMapping("")
     public  String  findPastOrder(Model model){
         Integer userId = (Integer) session.getAttribute("userId");
             if(userId==null){
                 return  "forward:/login";
             }else{
-
-
-
-                
-                return "order_past";
+                List<Order> orderList=service.findOrderHistory(userId);
+                if(orderList.size()==0){
+                    model.addAttribute("emptyMessage","カートに商品がありません");
+                }else if(orderList.size() !=0){
+                    model.addAttribute("orderHistory",orderList) ;
+                }
+                return "order_history";
             }
     }
 }
--- src/main/java/com/example/ecommerce_a/repository/OrderHisoryRepository.java
+++ src/main/java/com/example/ecommerce_a/repository/OrderHisoryRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -14,90 +13,75 @@ import org.springframework.stereotype.Repository;
 import com.example.ecommerce_a.domain.Item;
 import com.example.ecommerce_a.domain.Order;
 import com.example.ecommerce_a.domain.OrderItem;
-import com.example.ecommerce_a.domain.OrderTopping;
-import com.example.ecommerce_a.domain.Topping;
 
 @Repository
 public class OrderHisoryRepository {
 	
-	  //ResultSetオブジェクトに格納された複数行分のデータをList<Clab>変数にセットしてreturnする
-	  private static final RowMapper<Order> ORDER_ROW_MAPPER = (rs,i) -> {
-		  List<Order> orderList=new	ArrayList<>();
-	    //初めにデータを格納するための変数を宣言
-	    List<OrderItem> orderItemList = new ArrayList<>(); 
+		// ResultSetオブジェクトに格納された複数行分のデータをList<Clab>変数にセットしてreturnする
+		private static final ResultSetExtractor<List<Order>> ORDER_ITEM_RESULTSET = (rs) -> {
+			// 初めにデータを格納するための変数を宣言
+			List<Order> orderList = new ArrayList<>();
+	
+			// メンバーを格納するためのList<Member>変数を宣言(値はNullを格納しておく)
+			List<OrderItem> orderItemList = null;
+	
+			// clabsテーブルは結合した際に複数行にわたり同じデータが出力される可能性があるため、前のClabテーブルのIDを保持するための変数を宣言
+			int beforeIdNum = 0;
+	
+			// ResultSetオブジェクトに格納された複数のデータをList<Clab>変数に格納していく
+			while (rs.next()) {
+				// 現在検索しているClabテーブルのIDを格納するための変数を宣言
+				int nowIdNum = rs.getInt("ord_id");
+	
+				// 現在検索しているClabテーブルのIDと前のClabテーブルのIDが違う場合は新たにClabオブジェクトを作成する
+				if (nowIdNum != beforeIdNum) {
+					Order order = new Order();
+					order.setStatus(rs.getInt("ord_order_date"));
+					order.setOrderDate(rs.getDate("ord_date"));
+					order.setId(rs.getInt("ord_id"));
+					order.setTotalPrice(rs.getInt("ord_price"));
+					// メンバーがいた際にClabオブジェクトのmemberListに格納するため空のArrayListをセットしておく
+					orderItemList = new ArrayList<OrderItem>();
+					order.setOrderItemList(orderItemList);
+					orderList.add(order);
+				}
+				// ClabにMemberがいない場合はMemberオブジェクトを作成しないようにする
+				if (rs.getInt("ori_id") != 0) {
+					OrderItem orderItem = new OrderItem();
+					orderItem.setId(nowIdNum);
+					orderItem.setQuantity(rs.getInt("ori_quantity"));
+					char[] c = rs.getString("ori_size").toCharArray();
+					orderItem.setSize(c[0]);
+	
+					Item item = new Item();
+					item.setName(rs.getString("itm_name"));
+					item.setPriceM(rs.getInt("itm_price_m"));
+					item.setPriceL(rs.getInt("itm_price_l"));
+					item.setImagePath(rs.getString("itm_image_path"));
+					orderItem.setItem(item);
+					// メンバーがいた際にClabオブジェクトのmemberListに格納するため空のArrayListをセットしておく
+					orderItemList.add(orderItem);
+				}
+	
+			  //現在検索しているClabテーブルのIDを前のClabテーブルのIDを入れるbeforeIdNumに代入する
+			  beforeIdNum = nowIdNum;
+			}
+			return orderList;
+		  };
 
-	    //メンバーを格納するためのList<Member>変数を宣言(値はNullを格納しておく)
-	    List<OrderTopping>toppingList = new ArrayList<>();
 
-		Order order=new	Order();
-		order.setId(rs.getInt("user_id"));
-		order.
-
-
-
-	        OrderItem orderItem = new OrderItem();
-	        orderItem.setId(nowIdNum);
-	        orderItem.setQuantity(rs.getInt("ori_quantity"));
-	        char[] c = rs.getString("ori_size").toCharArray();
-	        orderItem.setSize(c[0]);
-	        
-	        Item item = new Item();
-	        item.setName(rs.getString("itm_name"));
-	        item.setPriceM(rs.getInt("itm_price_m"));
-	        item.setPriceL(rs.getInt("itm_price_l"));
-	        item.setImagePath(rs.getString("itm_image_path"));
-	        orderItem.setItem(item);
-	        //メンバーがいた際にClabオブジェクトのmemberListに格納するため空のArrayListをセットしておく
-	        toppingList = new ArrayList<OrderTopping>();
-	        orderItem.setOrderToppingList(toppingList);
-	        orderItemList.add(orderItem);
-	      
-
-	      //ClabにMemberがいない場合はMemberオブジェクトを作成しないようにする
-	      if (rs.getInt("top_id") != 0) {
-	        OrderTopping orderTopping = new OrderTopping();
-	        Topping topping = new Topping();
-	        topping.setName(rs.getString("top_name"));
-	        topping.setPriceM(rs.getInt("top_price_m"));
-	        topping.setPriceL(rs.getInt("top_price_l"));
-	        orderTopping.setTopping(topping);
-	        //orderTopping.set(rs.getString("m_name"));
-	        //memberをclabオブジェクト内にセットされているmemberListに直接追加する
-	        toppingList.add(orderTopping);
-	      }
-
-
-	      //現在検索しているClabテーブルのIDを前のClabテーブルのIDを入れるbeforeIdNumに代入する
-	      beforeIdNum = nowIdNum;
-	    }
-	    return orderList;
-	  };
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
-
-
-
-
-	  /**
-		   * 注文履歴で日付を参照処理
-		   * ROW_MAPPERに日付を格納
-		   */
-		  if(rs.getInt("order_date")!= 0){
-			Order order=new Order();
-
-		}
-
 
 	
 	public	List<Order> findOrderHistory(int userId){
-		String	sql="SELECT ord.delivery_time ord_delivery_time, ori.id ori_id,ori.quantity ori_quantity,ori.size ori_size,itm.name itm_name,itm.price_m itm_price_m,itm.price_l itm_price_l,itm.image_path itm_image_path,top.name top_name,top.price_m top_price_m,top.price_l top_price_l,top.id top_id "
+		String	sql="SELECT ord.status ord_status,ord.total_price ord_total_price,ord,ord.order_date ord_order_date,"
+		+" ord.id ord_id,ori.id ori_id,ori.quantity ori_quantity,ori.size ori_size,itm.name itm_name,itm.price_m itm_price_m,itm.price_l itm_price_l,itm.image_path itm_image_path "
 		+ "FROM orders ord "
 		+ "JOIN order_items ori ON ord.id=ori.order_id "
 		+ "JOIN items itm ON ori.item_id = itm.id "
-		+ "LEFT OUTER JOIN order_toppings ort ON ori.id = ort.order_item_id "
-		+ "LEFT OUTER JOIN toppings top ON ort.topping_id = top.id "
-		+ "WHERE ord.user_id=:userId AND (ord.status=1 OR ord.status2);";
+		+ "WHERE ord.user_id=:userId AND (ord.status=1 OR ord.status=2);";
+
 
 		
 		SqlParameterSource param = new MapSqlParameterSource().addValue("userId",userId);
